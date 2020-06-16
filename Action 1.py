74bef09d1f32f54284267d8e7b40b6216f157331
yanxiwanwwan <67015669+yanxiwanwwan@users.noreply.github.com>
2020-06-17 00:19:32 +0800

    L1

---
+++ Action 1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 19:39:59 2020
+
+@author: XuJing3
+"""
+
+a = 0
+for number in range(51):
+    a = a + number*2
+print (a)
+
+
+a=0
+b=2
+while b < 101:
+    a=a+b
+    b=b+2
+print (a)
